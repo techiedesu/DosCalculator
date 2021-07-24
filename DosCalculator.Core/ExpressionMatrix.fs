c8bd7dd2261a2544df51c2f9a26a2e3e1437624d
Vladislav Dyshakov <techiedesu@protonmail.com>
2021-07-25 06:14:35 +0700

    Added SRTP for matrix type

--- DosCalculator.Core/ExpressionMatrix.fs
+++ DosCalculator.Core/ExpressionMatrix.fs
@@ -47,6 +47,54 @@ type Matrix with
                     x = this.x - 1 }
         else
             None
+            
+    member this.getNode x y =
+        this.nodes |> Array.find ^ fun node -> node.x = x && node.y = y
+           
+           
+module Matrix =
+    let create x y =
+        { x = x
+          y = y
+          nodes = Array.zeroCreate ^ x * y }
+        
+    let map (predicate: int -> int -> Expression -> Expression) (matrix: Matrix) : Matrix =
+        let nodes = matrix.nodes
+                    |> Array.map ^ fun node ->
+                        { x = node.x
+                          y = node.y
+                          expression = predicate node.x node.y node.expression }
+        { x = matrix.x
+          y = matrix.y
+          nodes = nodes }
+
+module SRTP =
+    let act (matrix1: Matrix) (matrix2: Matrix) (action: Expression -> Expression -> Expression) =
+        let areMatricesSquare = matrix1.isSquare() && matrix2.isSquare()
+        let areDimensionsEquals = matrix1.x = matrix2.x
+        let matrixSize = matrix1.x
+        
+        if  areMatricesSquare && areDimensionsEquals then
+            let matrix = Matrix.create matrixSize matrixSize
+            let nodes = matrix.nodes
+                            |> Array.map ^ fun node ->
+                                let x = node.x
+                                let y = node.y
+                                let expression = action (matrix1.getNode x y).expression (matrix2.getNode x y).expression
+                                
+                                { x = x
+                                  y = y
+                                  expression = expression }
+                
+            Some { matrix with nodes = nodes }
+        else
+            None
+    
+    let inline (+) (matrix1: Matrix) (matrix2: Matrix) =
+        act matrix1 matrix2 (fun x y -> x + y)
+            
+    let inline (*) (matrix1: Matrix) (matrix2: Matrix) =
+        act matrix1 matrix2 (fun x y -> x * y)
 
 // TODO: Tests.
 let setExpression (matrix: Matrix) x y expression =
